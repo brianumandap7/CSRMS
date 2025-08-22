6a2f04c968d33e4f99816819ad4606c0049b80ee
Brian Jayson P. Umandap <brian.umandap@dotrgov.ph>
2025-08-22 14:33:20 +0800

    first commit and upload

---
+++ dbcreate/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Ticket_tbl, IR_tbl, Roles, Responder_tbl, Classification_tbl, Severity_tbl, TLP_tbl
+# Register your models here.
+
+
+admin.site.register(Ticket_tbl)
+admin.site.register(IR_tbl)
+admin.site.register(Roles)
+admin.site.register(Responder_tbl)
+admin.site.register(Classification_tbl)
+admin.site.register(Severity_tbl)
+admin.site.register(TLP_tbl)
