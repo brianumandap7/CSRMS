0feb0afeb87291594c574e466c47b3a356898357
rumberb1 <rumerb1>
2025-09-24 11:11:19 +0800

    API and landing page

--- dbcreate/urls.py
+++ dbcreate/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from .views import gdb
+from .views import gdb, rodb, classdb, sevdb, tlpdb, statdb, iprdb
 
 urlpatterns = [
     path('gdb/', views.gdb.as_view(), name='gdb'),
+    path('rodb/', views.rodb.as_view(), name='rodb'),
+    path('respdb/', views.respdb.as_view(), name='respdb'),
+    path('classdb/', views.classdb.as_view(), name='classdb'),
+    path('sevdb/', views.sevdb.as_view(), name='sevdb'),
+    path('tlpdb/', views.tlpdb.as_view(), name='tlpdb'),
+    path('statdb/', views.statdb.as_view(), name='statdb'),
+    path('iprdb/', views.iprdb.as_view(), name='iprdb'),
 ]
 
--- dbcreate/views.py
+++ dbcreate/views.py
@@ -1,15 +1,69 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Genders
+from .models import Genders, Roles, Responder_tbl, Classification_tbl, Severity_tbl, TLP_tbl, Ticket_tbl, Status_tbl, IR_tbl
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
-class gdb(APIView):
+class gdb(APIView): # dbcreate/gdb
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
         output = list(Genders.objects.values())
         return Response(output)
 
+class rodb(APIView): # dbcreate/rodb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(Roles.objects.values())
+        return Response(output)
 
+class respdb(APIView): # dbcreate/respdb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(Responder_tbl.objects.values())
+        return Response(output)
+
+class classdb(APIView): # dbcreate/classdb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(Classification_tbl.objects.values())
+        return Response(output)
+
+class sevdb(APIView): # dbcreate/sevdb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(Severity_tbl.objects.values())
+        return Response(output)
+
+class tlpdb(APIView): # dbcreate/tlpdb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(TLP_tbl.objects.values())
+        return Response(output)
+    
+class ticketdb(APIView): # dbcreate/ticketdb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(Ticket_tbl.objects.values())
+        return Response(output)
+
+class statdb(APIView): # dbcreate/statdb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(Status_tbl.objects.values())
+        return Response(output)
+
+class iprdb(APIView): # dbcreate/iprdb
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        output = list(IR_tbl.objects.values())
+        return Response(output)
